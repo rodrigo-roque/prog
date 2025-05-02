234356abef8b9821590b6c9cad1f4b437834c32c
rodrigo-roque <rodrigohroque@gmail.com>
2025-05-02 15:18:28 +0100

    private definidos

---
+++ cmake-build-debug/CMakeFiles/runscrim.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runscrim.dir/main/RunScrim.cpp.o"
+  "CMakeFiles/runscrim.dir/main/RunScrim.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Color.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Color.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Command.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Command.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Command/Blank.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Command/Blank.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Command/Open.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Command/Open.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Command/Save.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Command/Save.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Image.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Image.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Logger.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Logger.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/PNG.cpp.o"
+  "CMakeFiles/runscrim.dir/src/PNG.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Scrim.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Scrim.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/ScrimParser.cpp.o"
+  "CMakeFiles/runscrim.dir/src/ScrimParser.cpp.o.d"
+  "CMakeFiles/runscrim.dir/src/Utils.cpp.o"
+  "CMakeFiles/runscrim.dir/src/Utils.cpp.o.d"
+  "runscrim"
+  "runscrim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runscrim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/tester.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rodrigo_roque/project/main/Tester.cpp" "CMakeFiles/tester.dir/main/Tester.cpp.o" "gcc" "CMakeFiles/tester.dir/main/Tester.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Color.cpp" "CMakeFiles/tester.dir/src/Color.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Color.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Command.cpp" "CMakeFiles/tester.dir/src/Command.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Command/Blank.cpp" "CMakeFiles/tester.dir/src/Command/Blank.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Blank.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Command/Open.cpp" "CMakeFiles/tester.dir/src/Command/Open.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Open.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Command/Save.cpp" "CMakeFiles/tester.dir/src/Command/Save.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Save.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Image.cpp" "CMakeFiles/tester.dir/src/Image.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Image.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Logger.cpp" "CMakeFiles/tester.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Logger.cpp.o.d"
+  "/home/rodrigo_roque/project/src/PNG.cpp" "CMakeFiles/tester.dir/src/PNG.cpp.o" "gcc" "CMakeFiles/tester.dir/src/PNG.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Scrim.cpp" "CMakeFiles/tester.dir/src/Scrim.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Scrim.cpp.o.d"
+  "/home/rodrigo_roque/project/src/ScrimParser.cpp" "CMakeFiles/tester.dir/src/ScrimParser.cpp.o" "gcc" "CMakeFiles/tester.dir/src/ScrimParser.cpp.o.d"
+  "/home/rodrigo_roque/project/src/Utils.cpp" "CMakeFiles/tester.dir/src/Utils.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
