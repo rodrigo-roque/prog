20a28a283fdc5ab8d29643042a6d33326349b0a5
doping705 <francisco.c.lourenco@gmail.com>
2025-05-02 09:52:29 +0100

    cmake novo

---
+++ cmake-build-debug/CMakeFiles/tester.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tester.dir/main/Tester.cpp.o"
+  "CMakeFiles/tester.dir/main/Tester.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Color.cpp.o"
+  "CMakeFiles/tester.dir/src/Color.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Command.cpp.o"
+  "CMakeFiles/tester.dir/src/Command.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Command/Blank.cpp.o"
+  "CMakeFiles/tester.dir/src/Command/Blank.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Command/Open.cpp.o"
+  "CMakeFiles/tester.dir/src/Command/Open.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Command/Save.cpp.o"
+  "CMakeFiles/tester.dir/src/Command/Save.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Image.cpp.o"
+  "CMakeFiles/tester.dir/src/Image.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Logger.cpp.o"
+  "CMakeFiles/tester.dir/src/Logger.cpp.o.d"
+  "CMakeFiles/tester.dir/src/PNG.cpp.o"
+  "CMakeFiles/tester.dir/src/PNG.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Scrim.cpp.o"
+  "CMakeFiles/tester.dir/src/Scrim.cpp.o.d"
+  "CMakeFiles/tester.dir/src/ScrimParser.cpp.o"
+  "CMakeFiles/tester.dir/src/ScrimParser.cpp.o.d"
+  "CMakeFiles/tester.dir/src/Utils.cpp.o"
+  "CMakeFiles/tester.dir/src/Utils.cpp.o.d"
+  "tester"
+  "tester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
